5b49a55308632ee27641773ce61440f8de0877b5
calcpro <basnetsaki841@gmail.com>
2023-11-25 11:49:44 +0545

    Update on project_test code: Visualization support

--- project_test.py
+++ project_test.py
@@ -11,13 +11,34 @@ ray_direction = np.array(vector)
 thickness= trimesh.proximity.thickness(mesh, ray_origin, exterior=False, normals=None, method='ray')
 
 #gives the index of minimum thikness.
-index_small = thickness.argmin()
+index_small = thickness.min()
+list=[]
+min_ray=[]
+min_dir=[]
+
+for i in range(len(thickness)):
+    if thickness[i] < 1.5:
+        list.append(i)
+
+for j in list:
+    min_ray.append(ray_origin[j])
+    min_dir.append(ray_direction[j])
+
+min_ray = np.array(min_ray)
+min_dir = np.array(min_dir)
+
 print(thickness)
 
 #draws a line where the minimum thickness is encountered
-ray_visualize = trimesh.load_path(np.hstack((ray_origin[index_small],
-                                             ray_origin[index_small] + ray_direction[index_small]*2.0)).reshape(-1, 2, 3))
+ray_visualize = trimesh.load_path(np.hstack((min_ray,
+                                             min_ray + min_dir*2.0)).reshape(-1, 2, 3))
+
+# i=27
+# ray_visualize = trimesh.load_path(np.hstack((ray_origin[i],
+#                                              ray_origin[i] + ray_direction[i])).reshape(-1, 2, 3))
+
 mesh.visual.face_colors = [255,255,255,255]
+mesh.visual.face_colors[[21,22,27,28]] = [255, 0, 0, 255]
 scene = trimesh.Scene([mesh,ray_visualize])
 scene.show()
 
