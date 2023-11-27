1890a120c4fed0bbfaf2888e17bcc46ba149bf3c
paoneshrestha01 <151763722+paoneshrestha01@users.noreply.github.com>
2023-11-28 00:23:53 +0545

    all files run one by one

--- project_test-AllFilesRunOnce.py
+++ project_test-AllFilesRunOnce.py
@@ -2,53 +2,17 @@ import numpy as np
 import trimesh
 import os
 
-# import tkinter as tk
-# from tkinter import filedialog as fd
-# from tkinter import ttk
-# from tkinter.messagebox import showinfo
-
-# # create the root window
-# root = tk.Tk()
-# root.title('Tkinter Open File Dialog')
-# root.resizable(False, False)
-# root.geometry('300x150')
-# def select_file():
-#     filetypes = (
-#         ('text files', '*.txt'),
-#         ('All files', '*.*')
-#     )
-
-#     filename = fd.askopenfilename(
-#         title='Open a file',
-#         initialdir='/',
-#         filetypes=filetypes)
-
-#     showinfo(
-#         title='Selected File',
-#         message=filename
-#     )
-
-
-# # open button
-# open_button = ttk.Button(
-#     root,
-#     text='Open a File',
-#     command=select_file
-# )
-
-# open_button.pack(expand=True)
-
-
-# # run the application
-# root.mainloop()
-
 fileloc = 'C:/Users/Acer/PythonProjects/Internship_project-main/CAD_models'
 
+# All files in above directory are checked for min. thickness
 for root, dirs, files in os.walk(fileloc, topdown=False):
     for name in files:
         print(os.path.join(root, name))
         # filename = fd.askopenfilename()
         mesh = trimesh.load_mesh(os.path.join(root, name))
+
+        
+        # mesh = trimesh.load_mesh('C:/Users/Acer/PythonProjects/Internship_project-main/CAD_models/Rect1.stl')
 
         ray_origin = mesh.triangles_center
         vector = mesh.face_normals
@@ -58,15 +22,33 @@ for root, dirs, files in os.walk(fileloc, topdown=False):
         thickness= trimesh.proximity.thickness(mesh, ray_origin, exterior=False, normals=None, method='ray')
 
         #gives the index of minimum thikness.
-        index_small = thickness.argmin()
+        index_small = thickness.min()
+        list=[]
+        min_ray=[]
+        min_dir=[]
+
+        for i in range(len(thickness)):
+            if thickness[i] < 1.5:
+                list.append(i)
+
+        for j in list:
+            min_ray.append(ray_origin[j])
+            min_dir.append(ray_direction[j])
+
+        min_ray = np.array(min_ray)
+        min_dir = np.array(min_dir)
+
         print(thickness)
 
         #draws a line where the minimum thickness is encountered
-        ray_visualize = trimesh.load_path(np.hstack((ray_origin[index_small],
-                                                 ray_origin[index_small] + ray_direction[index_small]*2.0)).reshape(-1, 2, 3))
-        mesh.visual.face_colors = [128,255,255,255]
+        ray_visualize = trimesh.load_path(np.hstack((min_ray,
+                                                     min_ray + min_dir*2.0)).reshape(-1, 2, 3))
+
+        # i=27
+        # ray_visualize = trimesh.load_path(np.hstack((ray_origin[i],
+        #                                              ray_origin[i] + ray_direction[i])).reshape(-1, 2, 3))
+
+        mesh.visual.face_colors = [255,255,255,255]
+        mesh.visual.face_colors[[21,22,27,28]] = [255, 0, 0, 255]
         scene = trimesh.Scene([mesh,ray_visualize])
         scene.show()
-
-
-
